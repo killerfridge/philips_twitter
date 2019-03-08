b990de8a80eba871adced5d5b3a271c566261efa
Daniel Russell-Brain <daniel.rb@hotmail.co.uk>
2019-03-08 21:41:02 +0000

    Added a search function to the live streaming tweets

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import json
 import sqlite3
 import datetime as dt
 
-
+SEARCH_TERM = 'lumea'
 ckey = '174okOoY7G6cTh8DsDPgFsUCF'
 csecret = 'iRXkhd2welmWDMOTb8FLhMTypX7HDBmdpi2KFmceoDI2uhnh3L'
 atoken = '95300715-4QI5aoymwxfGUOgsoDFgwaXEsfG4pXhSsN0rj0wA7'
@@ -12,17 +12,21 @@ asecret = '7o6EAohXUQkVV4MCMTsOlXCHU862TgNdsHUhEhFMJPrRA'
 
 con = sqlite3.connect('sentiments.sqlite')
 
-con.execute('create table if not exists tweets (created datetime, tweet char, user char);')
+con.execute('drop table if exists tweets;')
+con.execute('create table if not exists tweets (created datetime, tweet char, user char, lang char, search char);')
 
 
 class Listener(StreamListener):
 
     def on_data(self, data):
         all_data = json.loads(data)
+        # print(all_data.keys())
         tweet = all_data['text']
         time = dt.datetime.now()
         user = all_data['user']['screen_name']
-        con.execute('insert into tweets (created, tweet, user) values (?, ?, ?)', (time, tweet, user))
+        lang = all_data['lang']
+        con.execute('insert into tweets (created, tweet, user, lang, search) values (?, ?, ?, ?, ?)',
+                    (time, tweet, user, lang, SEARCH_TERM))
         print(tweet)
         con.commit()
 
